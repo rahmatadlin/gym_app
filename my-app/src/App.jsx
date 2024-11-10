20f4436a39a2a8cd2e0742f8d2d2e7b5f00f0e53
naufalfikri0206 <Naufalfikri0206@gmail.com>
2024-11-10 12:01:29 +0700

    set up

---
+++ my-app/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="flex justify-center bg-slate-600 min-h-screen items-center">
+      <div>
+        WELLCOME
+      </div>
+    </div>
+  )
+}
+
+export default App
